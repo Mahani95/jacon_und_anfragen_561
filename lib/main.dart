28f6ba52a458198c578368e6ba64a50bbabb94d5
Driton Qerimi <bella@MBP-von-Driton.fritz.box>
2024-01-12 13:02:36 +0100

    main

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'SharedPreferences Demo',
+      title: 'SharedPreferences',
       home: MyWidget(),
     );
   }
